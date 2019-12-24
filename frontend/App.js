90e373dda3987e07b3ea2b82dd73f0f0fb23de4d
Lucas Bovolini <lbovolini94@gmail.com>
2019-12-24 19:07:16 -0300

    Adicionado tela de detalhes da partida

--- frontend/App.js
+++ frontend/App.js
@@ -3,11 +3,13 @@ import { createStackNavigator } from 'react-navigation-stack'
 
 import HomeScreen from './src/screens/HomeScreen'
 import SummonerScreen from './src/screens/SummonerScreen'
+import MatchDetailsScreen from './src/screens/MatchDetailsScreen'
 
 const navigator = createStackNavigator(
    {
       Home: HomeScreen,
       Summoner: SummonerScreen,
+      MatchDetails: MatchDetailsScreen,
    },
    {
       initialRouteName: 'Home',
---
+++ frontend/src/screens/MatchDetailsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const MatchDetailsScreen = () => {
+    return (
+        <View>
+            <Text>teste</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MatchDetailsScreen
